8ba3063e10c05239d23e49abfb2836f223909b01
Serega <ser.pisarenko@gmail.com>
2024-03-22 23:16:23 +0300

    практические задачи выполнены

--- Условия и циклы/Практические задания/Таблица умножения/main.py
+++ Условия и циклы/Практические задания/Таблица умножения/main.py
@@ -1,1 +1,7 @@
 # TODO Распечатать таблицу умножения
+size = 9
+for x in range(2, size + 1):
+    for y in range(2, size + 1):
+        space = '' if y == size else ' '
+        print(f"{x * y:>2}", end=space)
+    print()
