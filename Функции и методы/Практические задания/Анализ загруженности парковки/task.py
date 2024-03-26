01ea24d087bc0614eb144cafec9c15dce9ad96e8
Serega <ser.pisarenko@gmail.com>
2024-03-26 21:46:55 +0300

    Четыре практических задания к циклам выполнены

--- Функции и методы/Практические задания/Анализ загруженности парковки/task.py
+++ Функции и методы/Практические задания/Анализ загруженности парковки/task.py
@@ -1,1 +1,5 @@
 # TODO Напишите функцию `calculate_parking_load`
+def calculate_parking_load(total_parking_spaces, occupied_parking_spaces):
+    if total_parking_spaces >= occupied_parking_spaces:
+        return round((occupied_parking_spaces / total_parking_spaces) * 100)
+
--- Функции и методы/Функции. Создание функций/Передача аргументов в функцию и возвращаемые значения/main.py
+++ Функции и методы/Функции. Создание функций/Передача аргументов в функцию и возвращаемые значения/main.py
@@ -1,4 +1,14 @@
 # TODO Напишите функцию `multiply_numbers`
+def multiply_numbers(a, b):
+    product = a * b
+    return product
 
 
 # TODO Перемножте с помощью функции `multiply_numbers` при пары чисел и распечатайте результаты
+val_1 = multiply_numbers(2, 3)
+val_2 = multiply_numbers(4, 5)
+val_3 = multiply_numbers(6, 7)
+
+print(val_1)
+print(val_2,)
+print(val_3)
