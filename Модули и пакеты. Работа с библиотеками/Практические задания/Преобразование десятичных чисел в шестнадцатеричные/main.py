05b88e75ff694003521d43167076d214e9388ab0
admin <ser.pisarenko@gmail.com>
2024-04-04 22:37:29 +0300

    Сделано 9 практических из модули и пакеты.

--- Модули и пакеты. Работа с библиотеками/Практические задания/Преобразование десятичных чисел в шестнадцатеричные/main.py
+++ Модули и пакеты. Работа с библиотеками/Практические задания/Преобразование десятичных чисел в шестнадцатеричные/main.py
@@ -1,8 +1,10 @@
 # TODO Напишите функцию decimal_to_hex
 def decimal_to_hex() -> dict:
     """"Функция преобразовывает десятичные числа в шестнадцатеричные"""
-    return dict(val for val in enumerate(range(1, 16), 1))
+
+    return {dec_val: hex(dec_val) for dec_val in range(16)}
 
 
 if __name__ == '__main__':
-    print(decimal_to_hex())  # TODO Распечатайте словарь с десятичными и шестнадцатеричными числами
+    dec_to_hex_dict: dict = decimal_to_hex()
+    print(dec_to_hex_dict)  # TODO Распечатайте словарь с десятичными и шестнадцатеричными числами
--- Условия и циклы/Самостоятельная работа/task4/main.py
+++ Условия и циклы/Самостоятельная работа/task4/main.py
@@ -1,5 +1,10 @@
 # TODO завести счетчик для произведения
 # TODO посчитать произведение чисел
+last_count = 10
+num = 1
+for count in range(1, last_count + 1):
+    num *= count
+print(num)
 count = 1
 max_num = 10
 for num in range(1, max_num + 1):
