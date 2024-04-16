28c567333ed609b181f9e808585f70ef60cfd456
Serega <ser.pisarenko@gmail.com>
2024-04-16 11:01:04 +0300

    Лабораторные работы из источники данных выполнена

--- Работа с источниками данных/Лабораторная работа/Конвертер из CSV в JSON формат/main.py
+++ Работа с источниками данных/Лабораторная работа/Конвертер из CSV в JSON формат/main.py
@@ -1,14 +1,18 @@
 # TODO импортировать необходимые молули
-
+import csv
+import json
 
 INPUT_FILENAME = "input.csv"
 OUTPUT_FILENAME = "output.json"
 
 
-def task() -> None:
-    ...  # TODO считать содержимое csv файла
+def task(delimiter: str = ",", new_line: str = "\n") -> None:
+    """Функция конвертер из формата CSV в формат JSON"""
+    with open(INPUT_FILENAME, "r") as input_csv:
+        csv_data = [line for line in csv.DictReader(input_csv, delimiter=delimiter, quotechar=new_line)]
 
-    ...  # TODO Сериализовать в файл с отступами равными 4
+    with open(OUTPUT_FILENAME, "w", encoding="utf-8") as output_json:
+        json.dump(csv_data, output_json, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
